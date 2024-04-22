37e1ff161b4dc887ebe14ad42b5a13dd63112ffb
Zahid Hasan <zahidhasan140338@gmail.com>
2024-04-22 12:08:35 +0600

    styles has been integrated

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,42 +1,76 @@
 import { useEffect, useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import "./styles.css";
 
 export default function ImageSlider({ url, limit = 5, page = 1 }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const [images, setImages] = useState([]);
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [errorMsg, setErrorMsg] = useState(null);
-    const [loading, setLoading] = useState(false);
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
 
-    async function fetchImages(getUrl){
-        try{
-            setLoading(true)
-            const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
-            const data = await response.json();
-
-            if(data){
-                setImages(data);
-                setLoading(false);
-            }
-
-        }catch(e){
-            setErrorMsg(e.message);
-            setLoading(false);
-        }
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
     }
+  }
 
-    useEffect(()=>{
-        if(url !== "") fetchImages(url)
-    },[url]);
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
 
-    console.log(images);
+  console.log(images);
 
-    if(loading){
-        return <div>Loading data ! Please Wait</div>
-    }
+  if (loading) {
+    return <div>Loading data ! Please Wait</div>;
+  }
 
-    if(errorMsg !== null){
-        return <div>Error Occured !</div>
-    }
+  if (errorMsg !== null) {
+    return <div>Error Occured !</div>;
+  }
 
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill className="arrow arrow-left"></BsArrowLeftCircleFill>
+      {images && images.length
+        ? images.map((imageItem, index) => (
+            <img
+              key={imageItem.id}
+              alt={imageItem.download_url}
+              src={imageItem.download_url}
+              className={
+                currentSlide === index
+                  ? "current-image"
+                  : "current-image hide-current-image"
+              }
+            />
+          ))
+        : null}
+      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <span className="circle-indicators">
+        {images && images.length
+          ? images.map((_, index) => (
+              <button
+                key={index}
+                className={
+                  currentSlide === index
+                    ? "current-indicator"
+                    : "current-indicator inactive-indicator"
+                }
+                onClick={() => setCurrentSlide(index)}
+              ></button>
+            ))
+          : null}
+      </span>
+    </div>
+  );
 }
